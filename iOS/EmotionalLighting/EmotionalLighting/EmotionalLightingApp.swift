842807c88f2799aa25c4701e71b9b313604c1b20
b3lon9 <119420119+b3lon9@users.noreply.github.com>
2023-06-16 11:47:00 +0900

    iOS first commit

---
+++ iOS/EmotionalLighting/EmotionalLighting/EmotionalLightingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmotionalLightingApp.swift
+//  EmotionalLighting
+//
+//  Created by 김주환 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct EmotionalLightingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
